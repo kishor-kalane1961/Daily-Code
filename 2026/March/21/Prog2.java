938b49f686b3ae3a2ec828394d177de87184e59a
kavyanjali2025 <kavyanjalibhatt7@gmail.com>
2026-03-24 08:22:08 +0530

    Add main method to Prog2 class
    Added main method to Prog2 class to enable execution.

--- 2026/March/21/Prog2.java
+++ 2026/March/21/Prog2.java
@@ -1,5 +1,7 @@
 class Prog2{
+    public static void main(String[] args) {
     System.out.println("Kishor");
+}
 }
 /*Error: Could not find or load main class Prog2
 Caused by: java.lang.ClassNotFoundException: Prog2 */
