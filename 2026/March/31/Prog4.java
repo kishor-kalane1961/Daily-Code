04f5c6bc2c45cbe8b1ad447620ff86225b8f6ff0
kishor-kalane1961 <kishorkalane2005@gmail.com>
2026-03-31 06:20:09 +0530

    Practice Example of for loop and while loop

---
+++ 2026/March/31/Prog4.java
@@ -0,0 +1,11 @@
+class WhileExample2 {
+    public static void main(String[] args) {
+        int num = 5;
+        int i = 1;
+
+        while(i <= 10) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+            i++;
+        }
+    }
+}
